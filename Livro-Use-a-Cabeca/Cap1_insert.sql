d7c7ce7852df4ed078b1dcb0839f84d3c66c9d16
Kyara Massière <kyara.a.massiere@outlook.pt>
2026-02-07 11:38:07 -0300

    feat: estrutura inicial e primeiro insert do livro.

--- Livro-Use-a-Cabeca/Cap1_insert.sql
+++ Livro-Use-a-Cabeca/Cap1_insert.sql
@@ -15,3 +15,8 @@ DESC meus_contatos;
 -- Teste de inserção de dados
 INSERT INTO meus_contatos (sobrenome, primeiro_nome, email, sexo, aniversario, profissao, cidade, estado_civil, interesses, procura) 
 VALUES ('Anderson', 'Jillian', 'jill_anderson@gmail.com', 'F', '1980-09-05', 'Escritora Técnica', 'Palo Alto, CA', 'Solteiro', 'Caiaque, Répteis', 'Relacionamentos, Amigos');
+-- Teste de inserção de dados fora de ordem
+INSERT INTO meus_contatos (interesses, sexo, sobrenome, email, primeiro_nome, aniversario, cidade, profissao, estado_civil, procura)
+VALUES ('Ler, Cachoeiras', 'F', 'Granger', 'hermione_granger@gmail.com', 'Hermione', '1992-08-25', 'Londres', 'Aurora', 'Casada', 'Amigos, Atividades ao ar livre');
+-- Inserir apenas alguns dados 
+INSERT INTO meus_contatos (sobrenome, primeiro_nome, email) VALUES ('Angeles', 'Miguel', 'miguel_angeles@gmail.com');
